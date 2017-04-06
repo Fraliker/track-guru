aa0bbdf3e974116298b417dfd2c27193687f316f
Maciej Tatol <maciej.tatol@netguru.co>
2017-04-07 00:05:09 +0200

    added user assignment when creating new event

--- src/app/event-create/event-create.component.ts
+++ src/app/event-create/event-create.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
 import { AuthService } from '../providers/auth.service';
+import { IMultiSelectOption } from 'angular-2-dropdown-multiselect';
 import {
   SebmGoogleMap,
   SebmGoogleMapPolyline,
@@ -22,14 +23,16 @@ export class EventCreateComponent implements OnInit {
   navigator: any;
   getPositionId: void;
   destinationPoint: Point = {
-    lat: 51,
+    lat: 52.22,
     lng: 21
   };
   mapCenterLat: Point = {
-    lat: 50,
+    lat: 52.22,
     lng: 21
   };
-  mapZoom = 8;
+  mapZoom = 6;
+  optionsModel: number[] = [];
+  myOptions: any[];
 
 
   constructor(af: AngularFire, public authService: AuthService, public router: Router) {
@@ -76,22 +79,24 @@ export class EventCreateComponent implements OnInit {
 
   onSubmit(form: any): void {
     let selectedUsers = {}; // users IDs will be passed as object keys with value "true"
+    this.optionsModel.map(userId =>
+      selectedUsers[this.myOptions[userId].key] = true);
+    // users.map(user => selectedUsers[user.uid] = true); //adding users related to event
+    this.createdAt = new Date;
+    this.newEvent = {
+      created_at: this.createdAt.toString(),
+      created_by: this.uid,
+      event_name: form.eventName,
+      mapDestinationLat: this.destinationPoint.lat || 0,
+      mapDestinationLng: this.destinationPoint.lng || 0,
+      users: selectedUsers,
+    }
 
-    this.dbUsers.subscribe((users) => {
-      users.map(user => selectedUsers[user.uid] = true); //adding users related to event
-      this.createdAt = new Date;
-      this.newEvent = {
-        created_at: this.createdAt.toString(),
-        created_by: this.uid,
-        event_name: form.eventName,
-        mapDestinationLat: this.destinationPoint.lat || 0,
-        mapDestinationLng: this.destinationPoint.lng || 0,
-        users: selectedUsers,
-      }
+    console.log(selectedUsers);
 
-      this.dbEvents.push(this.newEvent);
-      this.router.navigate(['']);
-    });
+    this.dbEvents.push(this.newEvent);
+    this.router.navigate(['']);
+
   }
 
   updateTargetPosition(event) {
@@ -102,6 +107,24 @@ export class EventCreateComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.dbUsers.subscribe((users) => {
+      this.myOptions = users.map((user, index) => {
+        // event creator is added by default
+        if (user.uid === this.uid) {
+          this.optionsModel.push(index);
+        }
+
+        return {
+          id: index,
+          name: user.fullName,
+          key: user.uid,
+        }
+      }); //adding users
+    });
+  }
+
+  onChange() {
+    // handle for multiselect users change
   }
 
 }
