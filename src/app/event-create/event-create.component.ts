1f04caa38d9fbb67c00d08ccd63eda2de81b440c
Maciej Tatol <maciej.tatol@netguru.co>
2017-04-06 22:24:55 +0200

    added event create map event handling + geolocation on enter

--- src/app/event-create/event-create.component.ts
+++ src/app/event-create/event-create.component.ts
@@ -11,7 +11,7 @@ import {
 @Component({
   selector: 'app-event-create',
   templateUrl: './event-create.component.html',
-  styleUrls: ['./event-create.component.css']
+  styleUrls: ['./event-create.component.scss']
 })
 export class EventCreateComponent implements OnInit {
   dbEvents: FirebaseListObservable<any[]>;
@@ -19,6 +19,17 @@ export class EventCreateComponent implements OnInit {
   createdAt: Date;
   newEvent: Event;
   uid: string;
+  navigator: any;
+  getPositionId: void;
+  destinationPoint: Point = {
+    lat: 51,
+    lng: 21
+  };
+  mapCenterLat: Point = {
+    lat: 50,
+    lng: 21
+  };
+  mapZoom = 8;
 
 
   constructor(af: AngularFire, public authService: AuthService, public router: Router) {
@@ -29,6 +40,38 @@ export class EventCreateComponent implements OnInit {
         this.dbEvents = af.database.list('/events');
       }
     });
+
+    if (!navigator.geolocation) {
+       console.warn('Geolocation is not supported by your browser');
+    } else {
+      navigator.geolocation.getCurrentPosition(
+        this.success.bind(this),
+        this.error,
+        {
+          enableHighAccuracy: true,
+          timeout: 5000,
+          maximumAge: 0
+        }
+      );
+    };
+  }
+
+  success(position) {
+    // if at least one coordinate is provided
+    if (position.coords && position.coords.latitude)
+    {
+      this.destinationPoint = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+      //use the same starting values\
+      this.mapCenterLat = Object.assign({},this.destinationPoint);
+      this.mapZoom = 16;
+    }
+  }
+
+  error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
   }
 
   onSubmit(form: any): void {
@@ -41,13 +84,21 @@ export class EventCreateComponent implements OnInit {
         created_at: this.createdAt.toString(),
         created_by: this.uid,
         event_name: form.eventName,
-        map_destination_lat: form.latitude || 0,
-        map_destination_lng: form.longitude || 0,
+        mapDestinationLat: this.destinationPoint.lat || 0,
+        mapDestinationLng: this.destinationPoint.lng || 0,
         users: selectedUsers,
       }
 
-      this.dbEvents.push(this.newEvent).then(()=> this.router.navigate(['']));
+      this.dbEvents.push(this.newEvent);
+      this.router.navigate(['']);
     });
+  }
+
+  updateTargetPosition(event) {
+    this.destinationPoint = {
+      lat: event.coords.lat,
+      lng: event.coords.lng
+    }
   }
 
   ngOnInit() {
@@ -60,7 +111,12 @@ interface Event {
   created_by: string;
   event_started?: Date;
   event_name: string;
-  map_destination_lat: number;
-  map_destination_lng: number;
+  mapDestinationLat: number;
+  mapDestinationLng: number;
   users: any;
+}
+
+interface Point {
+  lat: number;
+  lng: number;
 }
